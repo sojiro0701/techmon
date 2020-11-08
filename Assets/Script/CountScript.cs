4c986491a3301fbf9473e194ae5e3788ee3bc78e
sojiro0701 <sojiro20060701@gmail.com>
2020-11-08 14:54:47 +0900

    finish

---
+++ Assets/Script/CountScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountScript : MonoBehaviour
+{
+    public Text text;
+    public Text damage;
+    public int number;
+
+    public int total;
+    public AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = 0;
+        sound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CountUP()
+    {
+        number = number + 1;
+        text.text = number.ToString();
+    }
+    public void Attack()
+    {
+        total += number;
+        damage.text = total.ToString();
+        sound.PlayOneShot(sound.clip);
+    }
+}
---
+++ Assets/Script/soundScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundScript : MonoBehaviour
+{
+    private static bool isCreated = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isCreated)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            isCreated = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
